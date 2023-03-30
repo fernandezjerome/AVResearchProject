b3040a2fc52d35395358f182c8fd3bcc1fb1a4e5
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2023-03-30 16:21:08 -0400

    red

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,16 @@
 var express = require("express");
-const { registerHelper } = require("hbs");
 var router = express.Router();
 
-let team = {
-    justin: {
-        name: "justin",
-        role: "developer",
-        image: "justin.jpg",
-    },
-
-    pawan: {
-        name: "pawan",
-        role: "cpprdinatore",
-    },
-};
-
-/* GET home page. */
 router.get("/", function (req, res) {
-    res.render("index", { title: "Express", content: "express is easy!" });
+    res.render("index");
 });
-router.get("/team/:member", function (req, res) {
-    console.log(req.params.member);
-    res.render("team", { person: team[req.params.member] });
+
+router.get("/", function (req, res) {
+    res.render("audio");
 });
+
+router.get("/", function (req, res) {
+    res.render("video");
+});
+
 module.exports = router;
